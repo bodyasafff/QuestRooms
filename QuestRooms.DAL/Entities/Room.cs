832e6c573845d6d4272ebd545525d779baa2d953
Богдан Сафнюк <bodyasafff@gmail.com>
2019-08-19 11:51:44 +0300

    hoh

---
+++ QuestRooms.DAL/Entities/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRooms.DAL.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeGoing { get; set; }
+        public virtual Address Address { get; set; }
+        public int MaxPlayers { get; set; }
+        public int MinPlayers { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Rating { get; set; }
+        public int LvlFear { get; set; }
+        public int LvlDifficulty { get; set; }
+        public string Logo { get; set; }
+        public virtual Company Company { get; set; }
+        public ICollection<Image> Images { get; set; }
+    }
+}
--- QuestRooms.DAL/RoomsContext.cs
+++ QuestRooms.DAL/RoomsContext.cs
@@ -1,5 +1,6 @@
 namespace QuestRooms.DAL
 {
+    using QuestRooms.DAL.Entities;
     using System;
     using System.Data.Entity;
     using System.Linq;
@@ -9,7 +10,14 @@ namespace QuestRooms.DAL
         public RoomsContext()
             : base("name=RoomsContext")
         {
-        }  
+        } 
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Street> Streets { get; set; }
          
     }
 }
